337b7daf0eab036f944286d338c935084819a365
Vitaliy Pustovyy <vetalsd2@gmail.com>
2017-02-07 19:31:16 +0200

    7.02 adaptive tablet

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	// remove placeholder
-	/*$(document).ready(function () {
+
 		$('input, textarea').focus(function(){
 			$(this).data('placeholder',$(this).attr('placeholder'))
 			$(this).attr('placeholder','');
@@ -8,14 +8,9 @@ $(document).ready(function() {
 		$('input, textarea').blur(function(){
 			$(this).attr('placeholder', $(this).data('placeholder'));
 		});
- 	});*/
-	/*
 	// ScrollTo
-	$('.navbar-collapse a').mPageScroll2id();
-	*/
-
-
-	//popup
+	$('.scrollto').mPageScroll2id();
+	// popup
 	$('.popup').magnificPopup({
 		type: 'inline'
 	});
@@ -37,6 +32,12 @@ $(document).ready(function() {
 		dots: false,
 		responsive: [
 			{
+			breakpoint: 1200,
+			settings: {
+				slidesToShow: 4
+			}
+			},
+			{
 			breakpoint: 992,
 			settings: {
 				slidesToShow: 3
@@ -44,6 +45,25 @@ $(document).ready(function() {
 			}
 		]
 	});
+	$('.review_slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		prevArrow: '.review_prev',
+		nextArrow: '.review_next',
+		dots: true,
+		customPaging: function(slider, i) {
+			return '<div class="dot"></div>';
+		},
+		responsive: [
+			{
+			breakpoint: 992,
+			settings: {
+				dots: false
+			}
+			}
+		]
+	});
+	
 	/*
 	// photo magnific gallery
 	$('.photo_popup').magnificPopup({
@@ -72,9 +92,9 @@ $(document).ready(function() {
 	});
 	*/
 
-	/*
+	
 	//validation
-	$('#').validate({
+	$('#topForm').validate({
 		rules: {
 			name: {
 				required: true,
@@ -104,8 +124,636 @@ $(document).ready(function() {
 			}
 		}
 	});
-
-	*/
+	$('#mainForm1').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: "Обязательно для заполения",
+				minlength: "От 2 до 16 букв",
+				maxlength: "От 2 до 16 букв"
+			},
+			email: {
+				required: "Обязательно для заполения",
+				email: "Введите корректный E-mail"
+			},
+			phone: {
+				required: "Введите корректный номер"
+			}
+		}
+	});
+	$('#mainForm2').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: "Обязательно для заполения",
+				minlength: "От 2 до 16 букв",
+				maxlength: "От 2 до 16 букв"
+			},
+			email: {
+				required: "Обязательно для заполения",
+				email: "Введите корректный E-mail"
+			},
+			phone: {
+				required: "Введите корректный номер"
+			}
+		}
+	});
+	$('#romeForm1').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: "Обязательно для заполения",
+				minlength: "От 2 до 16 букв",
+				maxlength: "От 2 до 16 букв"
+			},
+			email: {
+				required: "Обязательно для заполения",
+				email: "Введите корректный E-mail"
+			},
+			phone: {
+				required: "Введите корректный номер"
+			}
+		}
+	});
+	$('#romeForm2').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: "Обязательно для заполения",
+				minlength: "От 2 до 16 букв",
+				maxlength: "От 2 до 16 букв"
+			},
+			email: {
+				required: "Обязательно для заполения",
+				email: "Введите корректный E-mail"
+			},
+			phone: {
+				required: "Введите корректный номер"
+			}
+		}
+	});
+	$('#hammamForm1').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: "Обязательно для заполения",
+				minlength: "От 2 до 16 букв",
+				maxlength: "От 2 до 16 букв"
+			},
+			email: {
+				required: "Обязательно для заполения",
+				email: "Введите корректный E-mail"
+			},
+			phone: {
+				required: "Введите корректный номер"
+			}
+		}
+	});
+	$('#hammamForm2').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: "Обязательно для заполения",
+				minlength: "От 2 до 16 букв",
+				maxlength: "От 2 до 16 букв"
+			},
+			email: {
+				required: "Обязательно для заполения",
+				email: "Введите корректный E-mail"
+			},
+			phone: {
+				required: "Введите корректный номер"
+			}
+		}
+	});
+	$('#vatsuForm1').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: "Обязательно для заполения",
+				minlength: "От 2 до 16 букв",
+				maxlength: "От 2 до 16 букв"
+			},
+			email: {
+				required: "Обязательно для заполения",
+				email: "Введите корректный E-mail"
+			},
+			phone: {
+				required: "Введите корректный номер"
+			}
+		}
+	});
+	$('#vatsuForm2').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: "Обязательно для заполения",
+				minlength: "От 2 до 16 букв",
+				maxlength: "От 2 до 16 букв"
+			},
+			email: {
+				required: "Обязательно для заполения",
+				email: "Введите корректный E-mail"
+			},
+			phone: {
+				required: "Введите корректный номер"
+			}
+		}
+	});
+	$('#massageForm1').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: "Обязательно для заполения",
+				minlength: "От 2 до 16 букв",
+				maxlength: "От 2 до 16 букв"
+			},
+			email: {
+				required: "Обязательно для заполения",
+				email: "Введите корректный E-mail"
+			},
+			phone: {
+				required: "Введите корректный номер"
+			}
+		}
+	});
+	$('#massageForm2').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: "Обязательно для заполения",
+				minlength: "От 2 до 16 букв",
+				maxlength: "От 2 до 16 букв"
+			},
+			email: {
+				required: "Обязательно для заполения",
+				email: "Введите корректный E-mail"
+			},
+			phone: {
+				required: "Введите корректный номер"
+			}
+		}
+	});
+	$('#romeForm3').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: "Обязательно для заполения",
+				minlength: "От 2 до 16 букв",
+				maxlength: "От 2 до 16 букв"
+			},
+			email: {
+				required: "Обязательно для заполения",
+				email: "Введите корректный E-mail"
+			},
+			phone: {
+				required: "Введите корректный номер"
+			}
+		}
+	});
+	$('#paradiseForm').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: "Обязательно для заполения",
+				minlength: "От 2 до 16 букв",
+				maxlength: "От 2 до 16 букв"
+			},
+			email: {
+				required: "Обязательно для заполения",
+				email: "Введите корректный E-mail"
+			},
+			phone: {
+				required: "Введите корректный номер"
+			}
+		}
+	});
+	$('#pregnantForm').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: "Обязательно для заполения",
+				minlength: "От 2 до 16 букв",
+				maxlength: "От 2 до 16 букв"
+			},
+			email: {
+				required: "Обязательно для заполения",
+				email: "Введите корректный E-mail"
+			},
+			phone: {
+				required: "Введите корректный номер"
+			}
+		}
+	});
+	$('#spaForm').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: "Обязательно для заполения",
+				minlength: "От 2 до 16 букв",
+				maxlength: "От 2 до 16 букв"
+			},
+			email: {
+				required: "Обязательно для заполения",
+				email: "Введите корректный E-mail"
+			},
+			phone: {
+				required: "Введите корректный номер"
+			}
+		}
+	});
+	$('#nirvanaForm').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: "Обязательно для заполения",
+				minlength: "От 2 до 16 букв",
+				maxlength: "От 2 до 16 букв"
+			},
+			email: {
+				required: "Обязательно для заполения",
+				email: "Введите корректный E-mail"
+			},
+			phone: {
+				required: "Введите корректный номер"
+			}
+		}
+	});
+	$('#idealForm').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: "Обязательно для заполения",
+				minlength: "От 2 до 16 букв",
+				maxlength: "От 2 до 16 букв"
+			},
+			email: {
+				required: "Обязательно для заполения",
+				email: "Введите корректный E-mail"
+			},
+			phone: {
+				required: "Введите корректный номер"
+			}
+		}
+	});
+	$('#cosmoForm').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: "Обязательно для заполения",
+				minlength: "От 2 до 16 букв",
+				maxlength: "От 2 до 16 букв"
+			},
+			email: {
+				required: "Обязательно для заполения",
+				email: "Введите корректный E-mail"
+			},
+			phone: {
+				required: "Введите корректный номер"
+			}
+		}
+	});
+	$('#nailsForm').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: "Обязательно для заполения",
+				minlength: "От 2 до 16 букв",
+				maxlength: "От 2 до 16 букв"
+			},
+			email: {
+				required: "Обязательно для заполения",
+				email: "Введите корректный E-mail"
+			},
+			phone: {
+				required: "Введите корректный номер"
+			}
+		}
+	});
+	$('#styleForm').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: "Обязательно для заполения",
+				minlength: "От 2 до 16 букв",
+				maxlength: "От 2 до 16 букв"
+			},
+			email: {
+				required: "Обязательно для заполения",
+				email: "Введите корректный E-mail"
+			},
+			phone: {
+				required: "Введите корректный номер"
+			}
+		}
+	});
+	$('#correctForm').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: "Обязательно для заполения",
+				minlength: "От 2 до 16 букв",
+				maxlength: "От 2 до 16 букв"
+			},
+			email: {
+				required: "Обязательно для заполения",
+				email: "Введите корректный E-mail"
+			},
+			phone: {
+				required: "Введите корректный номер"
+			}
+		}
+	});
+	$('#makeupForm').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: "Обязательно для заполения",
+				minlength: "От 2 до 16 букв",
+				maxlength: "От 2 до 16 букв"
+			},
+			email: {
+				required: "Обязательно для заполения",
+				email: "Введите корректный E-mail"
+			},
+			phone: {
+				required: "Введите корректный номер"
+			}
+		}
+	});
 
 
 });
